b6fe7f02c698ba97e70d3ed55f1f3c1fd9daaa89
crankyastrologer <ansh21csu519@ncuindia.edu>
2024-04-07 13:13:29 +0530

    first commit

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+    import { GradientButton, Img } from 'flowbite-svelte';
+    import { P } from 'flowbite-svelte';
+    import { Search, Button } from 'flowbite-svelte';
+  import { SearchOutline } from 'flowbite-svelte-icons';
+    </script>
+
+<div class="mx-auto max-w-3xl">
+<form class="flex gap-2">
+    <Search size="md" />
+    <GradientButton class="!p-2.5">
+      <SearchOutline class="w-5 h-5" />
+    </GradientButton>
+        </form></div>    
+    <P class="mb-3" weight="light" color="text-gray-950 dark:text-gray-100 max-w-96" justify>welcome
+        .</P>
---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,73 @@
+<script lang='ts'>
+  import { GradientButton, Heading, Span, Input, Textarea } from 'flowbite-svelte';
+  import { EyeOutline, EyeSlashOutline } from 'flowbite-svelte-icons';
+  let show = false;
+import * as yup from 'yup';
+let values = {
+    email: "",
+    password: "",
+    
+}; 
+let errors = {};
+
+  const handleSubmit = async () => {
+    try {
+      await schema.validate(values, { abortEarly: false });
+      alert(JSON.stringify(values, null, 2));
+      errors = {};
+    } catch (err:any) {
+      errors = err.inner.reduce((acc:any, err:any) => {
+        return { ...acc, [err.path]: err.message };
+      }, {});
+    }
+  };
+const getCharacterValidationError = (str: string) => {
+  return `Your password must have at least 1 ${str} character`;
+};
+const schema = yup.object().shape({
+  email: yup.string().required("Email is required")
+    .email("Email is invalid"),
+  password: yup.string().required("Password is required").min(8,"password must have at least 8 charaters").matches(/[0-9]/, getCharacterValidationError("digit"))
+    .matches(/[a-z]/, getCharacterValidationError("lowercase"))
+    .matches(/[A-Z]/, getCharacterValidationError("uppercase")),
+  });
+  const validationResult = schema
+    .validate(values, { abortEarly: false })
+    .then(() => {
+      alert(JSON.stringify(values, null, 2));
+    })
+    .catch((err) => {
+      console.log(err.errors);
+    });
+</script>
+<form on:submit|preventDefault={handleSubmit}>
+<div class="min-h-screen flex items-center justify-center bg-gray-100">
+  <div class="max-w-lg w-full p-8 bg-white rounded-lg shadow-lg">
+    <Heading tag="h2" class="text-center mb-4">
+      <Span gradient>Login</Span>
+    </Heading>
+    <div class="mb-4">
+      <Input id="email" name="email" bind:value={values.email} placeholder="Email" />
+      {#if errors.email}
+        <span class="error">{errors.email}</span>
+      {/if}
+    </div>
+    <div class="mb-4">
+      <Input type={show ? 'text' : 'password'} id="password" placeholder="password" bind:value={values.password}>
+      <button slot="left" on:click={() => (show = !show)} class="pointer-events-auto" type='button'>
+        {#if show}
+          <EyeOutline class="w-6 h-6" />
+        {:else}
+          <EyeSlashOutline class="w-6 h-6" />
+        {/if}
+      </button></Input>
+
+      {#if errors.password}
+        <span class="error">{errors.password}</span>
+      {/if}
+    </div>
+    
+    <GradientButton outline color="purpleToBlue" class="mt-4 w-full" type='submit'>Submit</GradientButton>
+  </div>
+</div>
+</form>
